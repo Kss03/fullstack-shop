cf1f6ee006a05fa7d42e5eb76f0221fb524d9a44
kss03 <suranov123w@gmail.com>
2023-02-18 22:07:41 +0100

    added the create function to the deviceController

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,9 +1,34 @@
+const uuid = require('uuid'); // import package to generate a unique id (for example, device image)
+const path = require('path'); // package to correct work with the filesystem for all operating systems
+const {Device} = require('../models/models'); // import device model from the models
+const ApiError = require('../error/ApiError'); // import ApiError
+
 // класс для логики соответствующего роутера с функциями к каждому запросу
 class DeviceController {
 
     // To create a new type
-    async create(req,res) {
+    async create(req, res, next) {
+        try {
+            const {name, price, brandId, typeId, info,} = req.body; // to receive the data from request body
+            const {img} = req.files; // to receive the image file
 
+            let fileName = uuid.v4() + ".jpg"; // create a filename for image, usage "v4" function from "uuid" and add ".img" file format
+            img.mv(path.resolve(__dirname, "..", 'static', fileName)) //с помощью функции mv() переместили в папку static        
+            const device = await Device.create({
+                name,
+                price,
+                brandId,
+                typeId,
+                img: fileName, // for the database, we post a filename like an image
+
+            });
+
+            return res.json(device);
+
+        } catch(e) { // в случае ошибки передаем параметром саму ошибку
+            next(ApiError.badRequest(e.message)); 
+        }
+        
     }
 
     // Function to get all devices from the database
--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ require('dotenv').config(); // import module dotenv to using .env
 const express = require('express'); //import express 
 const sequelize = require ('./db'); // import objecct from db.js
 const models = require('./models/models'); // import database models
+
+const fileUpload = require('express-fileupload'); // импорт пакета для работы с файлами
 const cors = require('cors'); // import cors function from cors package
 const router = require('./routes/index'); // import routers
 const errorHandler = require('./middleware/ErrorHandingMiddleware'); // import middleware
+const path = require('path'); // пакет для грамотной работы с путями файловой системы
 
 
 const PORT = process.env.PORT || 5000; //import PORT from .env or 5000
@@ -12,6 +15,8 @@ const PORT = process.env.PORT || 5000; //import PORT from .env or 5000
 const app = express(); // object declaration usage express function
 app.use(cors()); // передаем функцию cors в функцию use, которую вызываем у app
 app.use(express.json()); // передаем в app.use() express.json(), чтобы наше приложение могло парсить json
+app.use(express.static(path.resolve(__dirname, 'static'))); // указываем явно, что файлы статики нужно раздавать как статику (например картинки девайсов)
+app.use(fileUpload({})); // to register fileupload package
 app.use('/api', router); // 1 параметр: url по которому router должен обрабатываться, 2 пар-р: сам роутер
 
 // указываем замыкающий middleware, которым возвращаем ответ на клиент ! обязательно указывем последним !
